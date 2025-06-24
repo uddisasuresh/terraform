7ee98119e42a3c2a9cfcb305efb03bb4dbcd095c
uddisa, suresh <suresh.uddisa@ge.com>
2025-06-24 22:15:48 +0530

    updated var value

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_instance" "example" {
-  ami = "var.ami_value"
-  instance_type = "var.instance_type_value"
+resource "aws_instance" "this" {
+  ami                     = var.ami_value
+  instance_type           = var.instance_type_value
+  tags = {
+    Name = "HelloWorld"
+  }
 }
-
