d91aafad656e040166b88a8b5850984ed4e8fe51
uddisa, suresh <suresh.uddisa@ge.com>
2025-06-25 11:13:30 +0530

    created multi region example

---
+++ multi_regions/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+  alias = "us-east-1"
+}
+provider "aws" {
+  region = "us-east -2"
+  alias = "us-east-2"
+}
+
+resource "aws_instance" "this" {
+  ami                     = "ami-0e449927258d45bc4"
+  instance_type           = "t2.micro"
+  provider = aws.us-east-1
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+resource "aws_instance" "this" {
+  ami                     = "ami-06971c49acd687c30"
+  instance_type           = "t2.micro"
+  provider = aws.us-east-2
+  tags = {
+    Name = "HelloWorld"
+  }
+}
